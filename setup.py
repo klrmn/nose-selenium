8d9c31ea9239fd6f9f417132b8f454eee7bae217
Leah Klearman <lklrmn@gmail.com>
2013-04-22 10:07:14 -0700

    fix dependencies

--- setup.py
+++ setup.py
@@ -35,6 +35,5 @@ setup(
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7'],
-    install_requires=['selenium', 'nose'],
-    test_requires=['unittest2'],
+    install_requires=['selenium', 'nose', 'requests', 'unittest2'],
 )
